f458b19647a227f1eb44cbdcced2b0629935ca52
pragyakar <pragyakar75@gmail.com>
2021-10-09 23:13:48 +0545

    :sparkles:  Add icons for external links

--- src/pages/Home/components/Gallery/GalleryItem.tsx
+++ src/pages/Home/components/Gallery/GalleryItem.tsx
@@ -1,4 +1,5 @@
 import { getStaticImageUrl } from "utils/image";
+import { FiExternalLink, FiFolder } from "react-icons/fi";
 
 interface IGalleryItemProps {
   label: string;
@@ -9,7 +10,13 @@ interface IGalleryItemProps {
 const GalleryItem = (props: IGalleryItemProps) => {
   return (
     <div className="gallery-item">
-      <span className="gallery-item--label">{props.label}</span>
+      <div className="gallery-item--overlay">
+        <span>{props.label}</span>
+        <div className="gallery-item--icons">
+          <FiFolder className="external-icon" />
+          <FiExternalLink className="external-icon" />
+        </div>
+      </div>
       <img
         src={getStaticImageUrl(props.thumbnailUrl)}
         className="gallery-item--image"
